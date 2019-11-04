10571ced2d168475e3dcbb7abc4a468b9dc6c065
Topher <topherb99@gmail.com>
2019-11-04 15:54:50 +0000

    added bookmarks route

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -3,4 +3,9 @@ feature 'infrastructure' do
     visit('/')
     expect(page).to have_content('Hello, world')
   end
+
+  scenario 'Bookmarks route is rendered' do
+    visit('/bookmarks')
+    expect(page).to have_content('BOOKMARK MANAGER')
+  end
 end
