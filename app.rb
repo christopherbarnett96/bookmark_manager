9e08f443705ba483941665a1642b0a91b04dc360
Topher <topherb99@gmail.com>
2019-11-06 13:28:48 +0000

    added functionality of adding bookmarks

--- app.rb
+++ app.rb
@@ -9,7 +9,18 @@ class BookmarkManager < Sinatra::Base
 
   get '/bookmarks' do
     @bookmarks = BookmarkBase.all
-    erb :bookmarks
+    erb :"bookmarks/bookmarks"
+  end
+
+  get '/bookmarks/new' do
+    erb :"bookmarks/new"
+  end
+
+  post '/bookmarks' do
+    url = params['url']
+    connection = PG.connect(dbname: 'bookmark_manager_test')
+    connection.exec("INSERT INTO bookmarks (url) VALUES('#{url}')")
+    redirect '/bookmarks'
   end
 
   run! if app_file == $0
---
+++ spec/features/creating_bookmarks_spec.rb
@@ -0,0 +1,9 @@
+feature 'Creating bookmarks' do
+  scenario 'User can create bookmarks and see them' do
+    visit('/bookmarks/new')
+    fill_in('url', with: 'http://testbookmark.com')
+    click_button('Submit')
+
+    expect(page).to have_content 'http://testbookmark.com'
+  end
+end
